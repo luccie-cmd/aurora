1030dd60489e76d2e6a15eea103a0d66a8a21b4d
luccie-cmd <hoedje201@gmail.com>
2024-06-26 22:49:19 +0200

    Add licences to all files

--- kernel/libc/stdio.h
+++ kernel/libc/stdio.h
@@ -1,3 +1,7 @@
+// This software is licensed under the GPL 3.0 license.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+
 #pragma once
 #include <stdint.h>
 #include <stdarg.h>
