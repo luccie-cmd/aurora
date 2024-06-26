936ffec3b811cffa3c188379cc47f1c143ddc969
luccie-cmd <hoedje201@gmail.com>
2024-06-26 17:13:31 +0200

    Add HAL wrapper for all

---
+++ kernel/arch/hal/hal.c
@@ -0,0 +1,15 @@
+#include "hal.h"
+#include "gdt/gdt.h"
+#include "idt/idt.h"
+#include <arch/fb/fb.h>
+
+void HalInitializeModules(){
+    InitGDT();
+    InitIDT();
+    // InitISR();
+    // InitIRQ();
+    // InitFB();
+    // InitMMU();
+    // InitSyscall();
+    // InitKeyboard();
+}
