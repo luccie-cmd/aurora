e5e4979554e0db0b4936239f54f933ebf3b9d834
luccie-cmd <hoedje201@gmail.com>
2024-07-02 22:44:12 +0200

    add CPUID to arch/io/io.h

--- kernel/arch/io/io.cc
+++ kernel/arch/io/io.cc
@@ -3,11 +3,16 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
 
 #include "io.h"
+#include <cpuid.h>
 
 namespace arch{
 namespace io{
     void outb(uint16_t port, uint8_t value) {
         __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+    }
+    bool cpuid(uint32_t function, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t* edx){
+        __get_cpuid(function, eax, ebx, ecx, edx);
+        return true;
     }
 }
 }
