d949b3a49b803151f626920ef62aa977bba5a095
luccie-cmd <hoedje201@gmail.com>
2024-07-04 15:03:00 +0200

    Change panic to include the RSOD

--- kernel/arch/hal/idt/isr.c
+++ kernel/arch/hal/idt/isr.c
@@ -43,7 +43,6 @@ IsrHandler IsrHandlers[256];
 
 void segFault(Registers* regs){
     printf("Exception occured %s\n", exception_strings[regs->interrupt]);
-    printf("RSOD\n");
     Panic();
 
 }
--- kernel/arch/io/io.c
+++ kernel/arch/io/io.c
@@ -24,6 +24,7 @@ uint64_t read_msr(uint32_t msr) {
 }
 
 void Panic(){
+    printf("TODO: RSOD\n");
     while(1);
 }
 
