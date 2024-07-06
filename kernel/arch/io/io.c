854e5631d926afd55d874d73cc073ce98c1ffde6
luccie-cmd <hoedje201@gmail.com>
2024-07-06 16:53:49 +0200

    Add more IO instructions

--- kernel/arch/io/io.c
+++ kernel/arch/io/io.c
@@ -8,8 +8,35 @@
 #include "gsod/gsod.h"
 
 void outb(uint16_t port, uint8_t value) {
-    __asm__ volatile ("out %1, %0" : : "a"(value), "Nd"(port));
+    __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
+
+uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+void outw(uint16_t port, uint16_t val) {
+    __asm__ volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+uint16_t inw(uint16_t port) {
+    uint16_t ret;
+    __asm__ volatile ("inw %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+void outl(uint16_t port, uint32_t val) {
+    __asm__ volatile ("outl %0, %1" : : "a"(val), "Nd"(port));
+}
+
+uint32_t inl(uint16_t port) {
+    uint32_t ret;
+    __asm__ volatile ("inl %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
 bool cpuid(uint32_t function, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t* edx){
     __cpuid(function, eax, ebx, ecx, edx);
     return true;
@@ -32,10 +59,4 @@ void Panic(){
 
 void iowait(){
     outb(0x80, 0);
-}
-
-uint8_t inb(uint16_t port){
-    uint8_t value;
-    __asm__ volatile ("in %0, %1" : "=a"(value) : "Nd"(port));
-    return value;
 }
--- kernel/arch/io/io.h
+++ kernel/arch/io/io.h
@@ -79,10 +79,14 @@ enum CpuidFeatures{
     CPUID_FEAT_EDX_PBE          = 1 << 31
 };
 
-void outb(uint16_t port, uint8_t value);
 bool cpuid(uint32_t function, uint32_t *eax, uint32_t* ebx, uint32_t *ecx, uint32_t* edx);
 void Panic();
 uint64_t read_msr(uint32_t msr);
 void write_msr(uint32_t msr, uint64_t value);
 void iowait();
+void outb(uint16_t port, uint8_t val);
+void outw(uint16_t port, uint16_t val);
+void outl(uint16_t port, uint32_t val);
 uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
+uint32_t inl(uint16_t port);
